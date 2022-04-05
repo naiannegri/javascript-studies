ce0c1ff783fef63cec1d86c421d4d71c67725e6b
Naiane Negri <naiannegri@gmail.com>
2022-04-04 21:59:37 -0300

    books example

--- React/initial course/my-app/src/index.js
+++ React/initial course/my-app/src/index.js
@@ -2,13 +2,29 @@ import React from 'react'
 import ReactDom from 'react-dom'
 //Nested components, React tools
 
-function Tools(){
+function BooksList(){
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <section>
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+        </section>
     )
 }
 
-ReactDom.render(<Tools />, document.getElementById('root'))
+const Book = () => {
+  return <article>
+    <Image></Image>
+    <Title/>
+    <Author/>
+  </article>
 
+}
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/41D3enj6JVS._SX324_BO1,204,203,200_.jpg" alt=""/>
+const Title = () => <h1>The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma</h1>
+const Author = () => <h4> Bessel van der Kolk M.D.</h4>
+
+ReactDom.render(<BooksList />, document.getElementById('root'))

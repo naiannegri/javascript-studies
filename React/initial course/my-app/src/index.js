87eb4baabf0b1f8c328847ecf6e60fa980aeef43
Naiane Negri <naiannegri@gmail.com>
2022-04-07 19:37:30 -0300

    different components

--- React/initial course/my-app/src/index.js
+++ React/initial course/my-app/src/index.js
@@ -4,32 +4,46 @@ import ReactDom from 'react-dom'
 //css
 import './index.css';
 //Nested components, React tools
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/61NdJMwAThS._AC_UL320_.jpg',
+  title: 'The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma',
+  author: 'Bessel van der Kolk M.D.'
+}
+
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/611bb7KoLwL._AC_UL320_.jpg',
+  title: 'AQUA BOOK DISNEY BABY',
+  author: 'Culturama'
+}
+
+const thirdBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/61pj7RWKyVL._AC_UL160_SR160,160_.jpg',
+  title: 'AQUA BOOK BABY SHARK',
+  author: 'Culturama'
+}
 
 function BooksList(){
+  return (
+    <section className = 'bookList'>
+      <Book img ={firstBook.img} title={firstBook.title} author = {firstBook.author} />      
+      <Book img={secondBook.img} title = {secondBook.title} author = {secondBook.author} />
+      <Book img={thirdBook.img} title = {thirdBook.title} author = {thirdBook.author} />
+    
+    </section>
+  )
+}
+
+const Book = (props) => {
+  
     return (
-      <section class="bookList">
-          <Book />
-          <Book />
-          <Book />
-          <Book />
-          <Book />
-        </section>
+      <article className='book'>
+        <img src={props.img} alt=""/>
+        <h1>{props.title}</h1>
+        <h4>{props.author}</h4>
+      </article>
     )
 }
 
 
-
-const Book = () => {
-  return <article class="book">
-    <Image></Image>
-    <Title/>
-    <Author/>
-  </article>
-
-}
-
-const Image = () => <img src="https://m.media-amazon.com/images/I/61NdJMwAThS._AC_UL320_.jpg" alt=""/>
-const Title = () => <h1>The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma</h1>
-const Author = () => <h4> Bessel van der Kolk M.D.</h4>
 
 ReactDom.render(<BooksList />, document.getElementById('root'))
